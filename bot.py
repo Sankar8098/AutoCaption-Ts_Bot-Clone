a3babb8ca84b415a958e18eae6e74218a1c6bf63
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-01-19 23:43:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,21 @@
 import pyrogram
-
 import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
 import os
-
 from config import Config
-from pyrogram import Client 
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-class autocaption(Client):
-    
-    super(autocaption, self).__init__(
-    name="Captioner",
-    bot_token=Config.BOT_TOKEN,
-    api_id=Config.API_ID,
-    api_hash=Config.API_HASH,
-    workers=20,
-    plugins=dict(
-        root="Plugins"
-    )
-)
+class autocaption(pyrogram.Client):
 
+    def __init__(self):
+        super().__init__(
+            name="Captioner",
+            bot_token=Config.BOT_TOKEN,
+            api_id=Config.API_ID,
+            api_hash=Config.API_HASH,
+            workers=20,
+            plugins=dict(
+                root="Plugins"
+            )
+        )
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     autocaption().run()
-
