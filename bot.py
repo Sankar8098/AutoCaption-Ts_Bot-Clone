4cd2678c64b5c85475658fd799b444c43610b31a
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-01-19 23:39:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,17 +13,18 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 class autocaption(Client):
     
-    def __init__(self):
-        super().__init__(
-            session_name="Captioner",
-            bot_token = Config.BOT_TOKEN,
-            api_id = Config.API_ID,
-            api_hash = Config.API_HASH,
-            workers = 20,
-            plugins = dict(
-                root="Plugins"
-            )
-        )
+    super().__init__(
+    name="Captioner",
+    bot_token=Config.BOT_TOKEN,
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    workers=20,
+    plugins=dict(
+        root="Plugins"
+    )
+  
+ )
+        
 
 if __name__ == "__main__" :
     autocaption().run()
